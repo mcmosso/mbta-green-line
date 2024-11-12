7ae867091cdc3b1b46d6d305ea301d1d841ed37b
mcmosso <mosso.ma@northeastern.edu>
2024-11-11 22:39:54 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// This is where you'll add your D3.js code to create the visualizations
+
+function createVisualization() {
+    // Add your D3.js visualization code here
+    console.log("Visualization code goes here");
+}
+
+// Call the function to create the visualization
+createVisualization();
