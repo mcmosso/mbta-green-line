3af9822879d732acbf2a66017f9fddd3cd0a8d05
mcmosso <mosso.ma@northeastern.edu>
2024-11-11 22:42:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
-// This is where you'll add your D3.js code to create the visualizations
+// Initialize scrollama
+const scroller = scrollama();
 
-function createVisualization() {
-    // Add your D3.js visualization code here
-    console.log("Visualization code goes here");
+// Generic window resize listener event
+function handleResize() {
+    scroller.resize();
 }
 
-// Call the function to create the visualization
-createVisualization();
+// Scrollama event handlers
+function handleStepEnter(response) {
+    // response = { element, direction, index }
+    console.log(response);
+    // Add your update logic here
+    response.element.classList.add('is-active');
+
+    // Update visualization based on step
+    updateVisualization(response.index);
+}
+
+function handleStepExit(response) {
+    // response = { element, direction, index }
+    console.log(response);
+    response.element.classList.remove('is-active');
+}
+
+function setupStickyfill() {
+    d3.selectAll('.sticky').each(function() {
+        Stickyfill.add(this);
+    });
+}
+
+function init() {
+    setupStickyfill();
+
+    // Set up visualization
+    setupVisualization();
+
+    // 1. Setup the scroller passing options
+    // This will also initialize trigger observations
+    // 2. Bind scrollama event handlers (this can be chained like below)
+    scroller
+        .setup({
+            step: '#scrolly article .step',
+            offset: 0.5,
+            debug: false,
+        })
+        .onStepEnter(handleStepEnter)
+        .onStepExit(handleStepExit);
+
+    // 3. Setup resize event
+    window.addEventListener('resize', handleResize);
+}
+
+// Visualization functions
+function setupVisualization() {
+    // Set up your main visualization here
+    console.log('Setting up visualization');
+}
+
+function updateVisualization(index) {
+    // Update your visualization based on the current step
+    console.log('Updating visualization for step', index);
+}
+
+// Kick things off
+init();
